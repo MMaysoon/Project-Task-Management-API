d2b663a90fde365e4efc58730155adffee61a5e8
MMaysoon <100812757+MMaysoon@users.noreply.github.com>
2026-05-20 13:37:46 +0300

    Pagination

---
+++ ProjectManagement.Application/Dtos/Pagination/PaginationExtention.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjectManagement.Application.Dtos.Pagination
+{
+    public static class PaginationExtension
+    {
+        public static async Task<PagedList<T>> ToPagedListAsync<T>(this IQueryable<T> source,int pageNumber,int pageSize) where T : class
+        {
+            var count = await source.CountAsync();
+
+            var items = await source
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
